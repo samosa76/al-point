19f7cd1c014ccd3ebb446439dffd2a41f048cf72
unknown <muhammadmiftahusshiddiq@gmail.com>
2025-02-23 21:08:17 +0700

    create  Footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,7 +3,27 @@ import style from './Footer.module.css'
 function Footer() {
     return (
         <div className={style.container}>
-            <h2>Footer</h2>
+
+            <div className={style.footer_title}>
+                <div className={style.title}>
+                    <p>Pondok Pesantren Modern
+                        <br />IT Multimedia Al Fatih</p>
+                </div>
+                <div className={style.icone}>
+                    <div className={style.icon}></div>
+                    <div className={style.icon}></div>
+                    <div className={style.icon}></div>
+                </div>
+            </div>
+
+            <div className={style.div_hr}>
+                <hr />
+            </div>
+
+            <div className={style.title2}>
+                <p>School Of IT Multimedia</p>
+                <a href="https://itmultimediaalfatih.com/">www.itmultimediaalfatih.com</a>
+            </div>
         </div>
     )
 }
