cf7bb9894bd67548ae6b3e5280e03d903501f5e0
Nasiayamgoreng <abdurrohimkholid04@gmail.com>
2025-03-01 13:41:18 +0700

    merge with new commit

--- src/Component/Admin/HomeAdmin/HomeAdmin.js
+++ src/Component/Admin/HomeAdmin/HomeAdmin.js
@@ -6,13 +6,27 @@ function HomeAdmin() {
     return (
         <div className={style.admin_container}>
             <div className={style.admin_title}>
-                <div></div>
-                <div></div>
+                <div className={style.name_title}>
+                    <h1>Admin Dashboard</h1>
+                </div>
+                <div>
+                    <img src='image/admin/org_main_komputer' />
+                </div>
+
             </div>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className={style.admin_info}>
+                <div className={style.admin_img}></div>
+                <div className={style.admin_text}>
+                    <div className={style.desc_text}>
+                    <img src='image/admin/image_org' />
+                    <p> Lorem Ipsum is dummy text of the printing and
+                    typesetting the industry's standard</p>
+                    </div>
+                    <div className={style.button_text}>
+                    <p>Input Santri</p>
+                    </div>
+                </div>
+
             </div>
         </div>
 
