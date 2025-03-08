96615964a3c72330563a7b127e803a213b26a7f4
Mfthshdq <muhammadmiftahusshiddiq@gmail.com>
2025-03-08 23:31:29 +0700

    manage screen list reward

--- src/Component/ListReward/Listreward.js
+++ src/Component/ListReward/Listreward.js
@@ -6,19 +6,21 @@ function Listreward() {
         <div className={style.container_listreward}>
 
             <div className={style.listreward}>
+
                 <div className={style.title}>
                     <h1>List Reward</h1>
                 </div>
+
                 <div className={style.hr_div}>
                     <hr />
                 </div>
+
                 <div className={style.list}>
                     <div className={style.RewardLists}>
                         <RewardLists />
                         <RewardLists />
                     </div>
                 </div>
-
 
             </div>
         </div>
