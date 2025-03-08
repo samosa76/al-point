0ba6ded315cdc1d729454c8f6d4b1ef641e3126f
IM-ERROR <dhiyaulhaq80624@gmail.com>
2025-03-08 22:27:01 +0700

    creat reward

--- src/Component/Reward/Reward.js
+++ src/Component/Reward/Reward.js
@@ -1,3 +1,5 @@
+import RewardsContents from '../RewardContents/RewardContents';
+import Rewardspages from '../Rewardpages/RewardPages';
 import style from './Reward.module.css';
 
 function Rewards() {
@@ -17,28 +19,7 @@ function Rewards() {
                 </div>
 
             </div>
-
-            <div className={style.pages}>
-
-                <div className={style.ellipses}>
-
-                    <div className={style.ellipse}></div>
-
-                    <div className={style.ellipse2}>
-                        <div className={style.ellipse_2}></div>
-                    </div>
-
-                </div>
-
-                <div className={style.title}>
-                    <p>Nama</p>
-                    <p>Kelas</p>
-                    <p>Tanggal Lahir</p>
-                    <p>Alamat</p>
-                </div>
-
-            </div>
-
+            <Rewardspages />
         </div>
     )
 }
---
+++ src/Component/Rewardpages/RewardPages.js
@@ -0,0 +1,61 @@
+import RewardsContents from '../RewardContents/RewardContents';
+import style from './Rewardpages.module.css';
+
+function Rewardspages() {
+    return (
+
+        <div className={style.container_Reward}>
+            <div className={style.pages}>
+                <div className={style.page}>
+                <div className={style.photoSantri}>
+                        <img src='/image/PhotoSantri/Jibran.jpg' alt='image/jibran'></img>
+                    </div>
+                    <div className={style.ellipses}>
+                    
+                        <div className={style.ellipse}></div>
+
+                        <div className={style.ellipse2}>
+                            <div className={style.ellipse_2}></div>
+                        </div>
+
+                    </div>
+                    <div className={style.container_text}>
+
+                        <div className={style.text}>
+                            <div className={style.text_title}>
+                                <p>Nama</p>
+                            </div>
+                            <p>:</p>
+                        </div>
+                        <div className={style.text}>
+                            <div className={style.text_title}>
+                                <p>Kelas</p>
+                            </div>
+                            <p>:</p>
+                        </div>
+                        <div className={style.text}>
+                            <div className={style.text_title}>
+                                <p>Tanggal Lahir</p>
+                            </div>
+                            <p>:</p>
+                        </div>
+                        <div className={style.text}>
+                            <div className={style.text_title}>
+                                <p>Alamat</p>
+                            </div>
+                            <p>:</p>
+                        </div>
+
+                        <RewardsContents />
+                        
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Rewardspages;
